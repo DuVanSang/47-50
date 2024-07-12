bf2f6f6359e3d7b30f0d782e602969f255126eab
DuVanSang <146832513+DuVanSang@users.noreply.github.com>
2024-07-12 21:23:55 +0700

    Add files via upload

---
+++ bài49.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+public class Program
+{
+    public static void ModifyValue(ref int number)
+    {
+        number = 100;
+    }
+
+    public static void Main()
+    {
+        int myNumber = 0;
+        ModifyValue(ref myNumber);
+        Console.WriteLine(myNumber);
+    }
+}
